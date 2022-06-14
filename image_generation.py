ded27adc9dc938bd388cd31a0f3a1fbdb27741d6
Zach <zachnudels@gmail.com>
2022-06-14 14:20:16 +0200

    2 Object Image
    Updated image generation file to generate images with two
    different objects in a specified location.

--- image_generation.py
+++ image_generation.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List
 
 
 def generate_block(input_dim: int = 121,
@@ -7,17 +7,46 @@ def generate_block(input_dim: int = 121,
                    midpoint: Optional[Tuple[int]] = None,
                    figure_orientation: float = 1,
                    bg_orientation: float = 0) -> np.ndarray:
-    image = []
-    if midpoint is None:
-        midpoint = (input_dim / 2, input_dim / 2)
-    for x in range(input_dim):
-        row = []
-        for y in range(input_dim):
-            if (midpoint[0] - figure_dim[0] / 2) <= x <= (midpoint[0] + figure_dim[0] / 2) \
-                    and (midpoint[1] - figure_dim[1] / 2) <= y <= (midpoint[1] + figure_dim[1] / 2):
-                row.append(figure_orientation)
-            else:
-                row.append(bg_orientation)
-        image.append(row)
-    return np.array(image, dtype='float32')
+    image = np.full((input_dim, input_dim), bg_orientation)
 
+    if midpoint is None:
+        mid_x, mid_y = int(input_dim / 2), int(input_dim / 2)
+    else:
+        mid_x = midpoint[0]
+        mid_y = midpoint[1]
+    fig_x = int(figure_dim[0] / 2)
+    fig_y = int(figure_dim[1] / 2)
+
+    image[mid_x - fig_x: mid_x + fig_x, mid_y - fig_y: mid_y + fig_y] = figure_orientation
+    return image
+
+
+def generate_2_blocks(input_dim: int = 121,
+                      figure_dim: Tuple[int] = (25, 25),
+                      figure_orientations: List[float] = None,
+                      bg_orientation: float = 0,
+                      midpoints: List[Tuple[int]] = None) -> np.ndarray:
+    image = np.full((input_dim, input_dim), bg_orientation)
+    fig_x = int(figure_dim[0]/2)
+    fig_y = int(figure_dim[1]/2)
+    mid1_x, mid1_y = midpoints[0][0], midpoints[0][1]
+    mid2_x, mid2_y = midpoints[1][0], midpoints[1][1]
+
+    if fig_x == 0 and fig_y != 0:
+        image[mid1_x, mid1_y-fig_y: mid1_y+fig_y] = figure_orientations[0]
+    elif fig_x != 0 and fig_y == 0:
+        image[mid1_x-fig_x: mid1_x+fig_x, mid1_y] = figure_orientations[0]
+    elif fig_x == 0 and fig_y == 0:
+        image[mid1_x, mid1_y] = figure_orientations[0]
+    else:
+        image[mid1_x-fig_x: mid1_x+fig_x, mid1_y-fig_y: mid1_y+fig_y] = figure_orientations[0]
+
+    if fig_x == 0 and fig_y != 0:
+        image[mid2_x, mid2_y-fig_y: mid2_y+fig_y] = figure_orientations[1]
+    elif fig_x != 0 and fig_y == 0:
+        image[mid2_x-fig_x: mid2_x+fig_x, mid2_y] = figure_orientations[1]
+    elif fig_x == 0 and fig_y == 0:
+        image[mid2_x, mid2_y] = figure_orientations[1]
+    else:
+        image[mid2_x-fig_x: mid2_x+fig_x, mid2_y-fig_y: mid2_y+fig_y] = figure_orientations[1]
+    return image
